8fd3d58e097cf754c88b0547908b273ab6560013
Artimus <rajbhongde4@gmail.com>
2024-05-19 17:05:43 +0530

    save template

--- template.py
+++ template.py
@@ -2,10 +2,9 @@ import os
 from pathlib import Path
 import logging
 
-logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s')
+logging.basicConfig(level= logging.INFO, format= '[%(asctime)s]:%(message)s:')
 
 project_name = 'Red_Wine'
-base_path = Path('F:/Machine_Learning/Red_wine_ML')
 
 list_of_files = [
     f'src/{project_name}/__init__.py',
@@ -13,12 +12,12 @@ list_of_files = [
     f'src/{project_name}/utils/__init__.py',
     f'src/{project_name}/utils/common.py',
     f'src/{project_name}/config/__init__.py',
-    f'src/{project_name}/config/configurations.py',
+    f'src/{project_name}/config/configurations.py', 
     f'src/{project_name}/pipeline/__init__.py',
     f'src/{project_name}/entity/__init__.py',
     f'src/{project_name}/entity/config_entity.py',
     f'src/{project_name}/constants/__init__.py',
-    "config/config.yaml",
+    "config/config.yaml",  
     'params.yaml',
     'schema.yaml',
     'main.py',
@@ -29,17 +28,19 @@ list_of_files = [
     'templates/index.html'
 ]
 
-for relative_filepath in list_of_files:
-    filepath = base_path / relative_filepath  # Prepend the base path
-    filedir, filename = filepath.parent, filepath.name
+for filepath in list_of_files:
+    filepath  = Path(filepath)
+
+    filedir, filename = os.path.split(filepath)
 
     if filedir != '':
-        os.makedirs(filedir, exist_ok=True)
-        logging.info(f'Creating directory {filedir} for the file {filename}')
+        os.makedirs(filedir, exist_ok= True)
+        logging.info(f'creating directory ; {filedir} for the file {filename}')
 
-    if not filepath.exists() or filepath.stat().st_size == 0:
+    if(not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
         with open(filepath, 'w') as f:
             pass
-        logging.info(f'Creating empty file {filepath}')
+            logging.info(f'creating empty file {filepath}')
+
     else:
-        logging.info(f'File {filepath} already exists')
+        logging.info(f'file {filepath} already exists')
